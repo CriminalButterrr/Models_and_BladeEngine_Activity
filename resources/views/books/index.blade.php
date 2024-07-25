db2ce18fac678113401940a2862fb24cdbfa0fbc
CriminalButterrr <h.agwiking@gmail.com>
2024-07-25 22:48:04 +0800

    Create footer and header partials, table, route, and controller for the book view.

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Book List</title>
+</head>
+<body>
+    @include('partials.header')
+
+    <div class="py-12">
+        <p class="text-center text-9xl font-bold">Book list</p>
+        <div class="pt-16">
+            <table class="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+                <thead>
+                    <tr>
+                    <th class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">ISBN</th>
+                    <th class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Title</th>
+                    <th class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Author</th>
+                    <th class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Description</th>
+                    <th class="px-6 py-3 text-star text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Date Published</th>
+                    </tr>
+                </thead>
+                <tbody class="divide-y divide-gray-200 dark:divide-neutral-700">
+                    @foreach ($books as $book)
+                    <tr>
+                        <td class="px-6 py-4 text-sm font-medium text-gray-800 dark:text-neutral-200">{{$book->isbn}}</td>
+                        <td class="px-6 py-4 text-sm font-medium text-gray-800 dark:text-neutral-200">{{$book->title}}</td>
+                        <td class="px-6 py-4 text-sm font-medium text-gray-800 dark:text-neutral-200">{{$book->author}}</td>
+                        <td class="px-6 py-4 text-sm font-medium text-gray-800 dark:text-neutral-200 ">{{$book->description}}</td>
+                        <td class="px-6 py-4 text-sm font-medium text-gray-800 dark:text-neutral-200">{{$book->date_published}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        
+    </div>
+    
+            
+    @include('partials.footer')
+</body>
+</html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-black text-white py-4">
+    <div class="container mx-auto text-center">
+        <p>&copy; {{ date('Y') }} Harold's Book Store. All rights reserved.</p>
+    </div>
+</footer>
